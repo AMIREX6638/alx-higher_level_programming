9325958256295e88365f528d0accf134ba08d44c
AMIREX6638 <rayaami0966@gmail.com>
2023-06-14 02:51:56 +0300

    Task all

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// module.exports = class Rectangle {
+//   constructor (w, h) {
+// this.width = w;
+// this.height = h;
+//   }
+// };
+
+// OR
+class Rectangle {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+}
+  
+module.exports = Rectangle;
+  
